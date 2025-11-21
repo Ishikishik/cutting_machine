0a1fe548eb5d2a14657e88ab7d097acf3f075ba0
Kawauso <161272652+Ishikishik@users.noreply.github.com>
2025-11-21 15:29:30 +0900

    checkradと一応list2gcodeが完成滑らかにする、角度を制限する作業に移る

--- gcodegenerator/main.py
+++ gcodegenerator/main.py
@@ -3,14 +3,14 @@ from list2gcode.processor import (
     sort_curves_tsp,
     export_curve_csv,
     generate_rotandscale_curves,
-    genrad_lut,
+    genrad_kdtree,
 )
 """
 curve_list = capture_and_extract_curve_list(source="camera")
 """
 curve_list = capture_and_extract_curve_list(
     source="image",
-    image_path="/Users/kawashimasatoshishin/cutting_machine/gcodegenerator/fri.jpg"
+    image_path="/Users/kawashimasatoshishin/cutting_machine/gcodegenerator/five.png"
 )
 
 if curve_list is None:
@@ -23,17 +23,17 @@ else:
     final_curves = generate_rotandscale_curves(
         sorted_list,
         rotate_deg = 90,      # 90°回転
-        box_w = 70,          # ハガキ短辺
-        box_h = 103.6,          # ハガキ長辺
-        offset_x = -103.6/2,        # →方向に 10mm 移動
-        offset_y = 30,        # ↓方向に -5mm 移動
+        box_w = 100,          # ハガキ短辺
+        box_h = 148,          # ハガキ長辺
+        offset_x = -148/2,        # →方向に 10mm 移動
+        offset_y = 50,        # ↓方向に -5mm 移動
         decimal_digits = 3    # 小数点以下3桁
 )
-    result = genrad_lut(
-        final_curves,
-        lut_path="/Users/kawashimasatoshishin/cutting_machine/gcodegenerator/list2gcode/rad2xy.csv",
-        l1=65, l2=85, d=50, offset=25
-    )
+    result = genrad_kdtree(
+    final_curves,
+    lut_path="/Users/kawashimasatoshishin/cutting_machine/gcodegenerator/list2gcode/lut_tree.pkl"
+)
+
 
 
     # CSV に保存
